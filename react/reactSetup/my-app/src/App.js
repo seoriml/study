e5e2511246a1b4db5cf9777b1ecbf3fa46e9aff0
Seorim Lee <esr619@naver.com>
2024-08-15 16:41:50 +0900

    240814 리액트수업

--- react/reactSetup/my-app/src/App.js
+++ react/reactSetup/my-app/src/App.js
@@ -1,7 +1,16 @@
+import React from "react";
+
+const ColorText = ({ color }) => {
+  return <p style={{ color: color }}>색이 바뀌어요</p>;
+};
+
+// App 컴포넌트 정의
 function App() {
   return (
-    <div className="App">
-      <h1>test2</h1>
+    <div>
+      <ColorText color="red" />
+      <ColorText color="green" />
+      <ColorText color="blue" />
     </div>
   );
 }
