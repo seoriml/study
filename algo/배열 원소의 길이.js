e26744f70b0037882949bfcafc3b81d4017aaa69
seorim <esr619@naver.com>
2024-12-06 23:21:01 +0900

    배열 원소의 길이 함수 테스트

--- algo/배열 원소의 길이.js
+++ algo/배열 원소의 길이.js
@@ -2,5 +2,9 @@ function solution(strlist) {
     var answer = strlist.map(function(str) {
         return str.length;
     });
+    console.log(answer);
     return answer;
 }
+
+// 함수 테스트
+solution(["사과", "바나나", "체리"]); 
