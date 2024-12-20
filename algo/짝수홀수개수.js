e7cb3a30089adbedf387d926a388885fd418075b
seorim <esr619@naver.com>
2024-12-20 23:37:41 +0900

    예시호출코드 추가

--- algo/짝수홀수개수.js
+++ algo/짝수홀수개수.js
@@ -10,5 +10,11 @@ function solution(num_list) {
         }
     }
     
+    console.log("짝수의 개수:", answer[0]);  // 짝수의 개수 출력
+    console.log("홀수의 개수:", answer[1]);  // 홀수의 개수 출력
+    
     return answer;  // 짝수와 홀수의 개수를 담은 배열 반환
 }
+
+// 예시 호출
+solution([1, 2, 3, 4, 5, 6]);  // 콘솔에 결과를 출력합니다.
